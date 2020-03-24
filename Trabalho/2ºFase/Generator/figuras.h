205194e058e3f9181435d39a2b4d5ad3753a8529
Komatsu52 <gjaesteves@gmail.com>
2020-03-24 04:10:39 +0000

    Ja fiz o generator

---
+++ Trabalho/2ºFase/Generator/figuras.h
@@ -0,0 +1,23 @@
+#ifndef FIGURAS_FIGURAS_H
+#define FIGURAS_FIGURAS_H
+
+#include <vector>
+
+using namespace std;
+
+typedef struct point {
+    float x;
+    float y;
+    float z;
+} Point;
+
+Point makePoint(float radius, float angle, float height);
+Point makePointSphere (float radius, float beta, float alpha);
+Point makePointTorus(float radiusIn,float radiusOut,float beta,float alpha);
+vector<Point> generatePlane(float x, float z);
+vector<Point> generateBox(float x, float y, float z, int div);
+vector<Point> generateSphere(float radius, int slices, int stacks);
+vector<Point> generateCone(float radius, float height, int slices, int stacks);
+vector<Point> generateCylinder (float radius, float height, int slices, int stacks);
+vector<Point> generateTorus(float radiusIn,float radiusOut , int slices, int layers);
+#endif

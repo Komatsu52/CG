8bb249d7bf2cc08df96e63b777e23c3a87ac302b
marioreal <marioreal@users.noreply.github.com>
2020-03-31 04:08:58 +0100

    Add files via upload

--- TPs/06_Terrain/code/main.cpp
+++ TPs/06_Terrain/code/main.cpp
@@ -32,26 +32,26 @@ unsigned char *imageData;  // Array linear onde vão ficar o valores dos pixeis 
 
 void changeSize(int w, int h) {
 
-	// Prevent a divide by zero, when window is too short
-	// (you cant make a window with zero width).
-	if(h == 0)
-		h = 1;
+    // Prevent a divide by zero, when window is too short
+    // (you cant make a window with zero width).
+    if(h == 0)
+        h = 1;
 
-	// compute window's aspect ratio 
-	float ratio = w * 1.0 / h;
+    // compute window's aspect ratio 
+    float ratio = w * 1.0 / h;
 
-	// Reset the coordinate system before modifying
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	
-	// Set the viewport to be the entire window
+    // Reset the coordinate system before modifying
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    
+    // Set the viewport to be the entire window
     glViewport(0, 0, w, h);
 
-	// Set the correct perspective
-	gluPerspective(45,ratio,1,1000);
+    // Set the correct perspective
+    gluPerspective(45,ratio,1,1000);
 
-	// return to the model view matrix mode
-	glMatrixMode(GL_MODELVIEW);
+    // return to the model view matrix mode
+    glMatrixMode(GL_MODELVIEW);
 }
 
 void carregarImagem(){
@@ -127,12 +127,12 @@ void Build_VBOterreno(float largura,float altura){
 
 void init() {
 
-// 	Load the height map "terreno.jpg"
+//  Load the height map "terreno.jpg"
     carregarImagem();
-// 	Build the vertex arrays
+//  Build the vertex arrays
     Build_VBOterreno(tw,th);
 
-// 	OpenGL settings
+//  OpenGL settings
     glEnable(GL_DEPTH_TEST);
     //glEnable(GL_CULL_FACE);
     glPolygonMode(GL_FRONT, GL_LINE);
@@ -155,27 +155,27 @@ void drawTerrain() {
 
 void renderScene(void) {
 
-	float pos[4] = {-1.0, 1.0, 1.0, 0.0};
+    float pos[4] = {-1.0, 1.0, 1.0, 0.0};
 
-	glClearColor(0.0f,0.0f,0.0f,0.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.0f,0.0f,0.0f,0.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glLoadIdentity();
-	gluLookAt(camX, camY, camZ,
-		      0.0,0.0,0.0,
-			  0.0f,1.0f,0.0f);
+    glLoadIdentity();
+    gluLookAt(camX, camY, camZ,
+              0.0,0.0,0.0,
+              0.0f,1.0f,0.0f);
 
     glColor3f(1.0f, 1.0f, 1.0f);
     ////desenho do terreno
     drawTerrain();
 
-	// just so that it renders something before the terrain is built
-	// to erase when the terrain is ready
+    // just so that it renders something before the terrain is built
+    // to erase when the terrain is ready
     glColor3f(0.0f, 0.0f, 1.0f);
-	glutWireTeapot(2.0);
+    glutWireTeapot(2.0);
 
 // End of frame
-	glutSwapBuffers();
+    glutSwapBuffers();
 }
 
 void processKeys(unsigned char key, int xx, int yy) {
@@ -185,88 +185,88 @@ void processKeys(unsigned char key, int xx, int yy) {
 
 void processMouseButtons(int button, int state, int xx, int yy) {
 
-	if (state == GLUT_DOWN)  {
-		startX = xx;
-		startY = yy;
-		if (button == GLUT_LEFT_BUTTON)
-			tracking = 1;
-		else if (button == GLUT_RIGHT_BUTTON)
-			tracking = 2;
-		else
-			tracking = 0;
-	}
-	else if (state == GLUT_UP) {
-		if (tracking == 1) {
-			alpha += (xx - startX);
-			beta += (yy - startY);
-		}
-		else if (tracking == 2) {
+    if (state == GLUT_DOWN)  {
+        startX = xx;
+        startY = yy;
+        if (button == GLUT_LEFT_BUTTON)
+            tracking = 1;
+        else if (button == GLUT_RIGHT_BUTTON)
+            tracking = 2;
+        else
+            tracking = 0;
+    }
+    else if (state == GLUT_UP) {
+        if (tracking == 1) {
+            alpha += (xx - startX);
+            beta += (yy - startY);
+        }
+        else if (tracking == 2) {
 
-			r -= yy - startY;
-			if (r < 3)
-				r = 3.0;
-		}
-		tracking = 0;
-	}
+            r -= yy - startY;
+            if (r < 3)
+                r = 3.0;
+        }
+        tracking = 0;
+    }
 }
 
 void processMouseMotion(int xx, int yy) {
 
-	int deltaX, deltaY;
-	int alphaAux, betaAux;
-	int rAux;
+    int deltaX, deltaY;
+    int alphaAux, betaAux;
+    int rAux;
 
-	if (!tracking)
-		return;
+    if (!tracking)
+        return;
 
-	deltaX = xx - startX;
-	deltaY = yy - startY;
+    deltaX = xx - startX;
+    deltaY = yy - startY;
 
-	if (tracking == 1) {
+    if (tracking == 1) {
 
 
-		alphaAux = alpha + deltaX;
-		betaAux = beta + deltaY;
+        alphaAux = alpha + deltaX;
+        betaAux = beta + deltaY;
 
-		if (betaAux > 85.0)
-			betaAux = 85.0;
-		else if (betaAux < -85.0)
-			betaAux = -85.0;
+        if (betaAux > 85.0)
+            betaAux = 85.0;
+        else if (betaAux < -85.0)
+            betaAux = -85.0;
 
-		rAux = r;
-	}
-	else if (tracking == 2) {
+        rAux = r;
+    }
+    else if (tracking == 2) {
 
-		alphaAux = alpha;
-		betaAux = beta;
-		rAux = r - deltaY;
-		if (rAux < 3)
-			rAux = 3;
-	}
-	camX = rAux * sin(alphaAux * 3.14 / 180.0) * cos(betaAux * 3.14 / 180.0);
-	camZ = rAux * cos(alphaAux * 3.14 / 180.0) * cos(betaAux * 3.14 / 180.0);
-	camY = rAux * 							     sin(betaAux * 3.14 / 180.0);
+        alphaAux = alpha;
+        betaAux = beta;
+        rAux = r - deltaY;
+        if (rAux < 3)
+            rAux = 3;
+    }
+    camX = rAux * sin(alphaAux * 3.14 / 180.0) * cos(betaAux * 3.14 / 180.0);
+    camZ = rAux * cos(alphaAux * 3.14 / 180.0) * cos(betaAux * 3.14 / 180.0);
+    camY = rAux *                                sin(betaAux * 3.14 / 180.0);
 }
 
 int main(int argc, char **argv) {
 
 // init GLUT and the window
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
-	glutInitWindowPosition(100,100);
-	glutInitWindowSize(320,320);
-	glutCreateWindow("CG@DI-UM");
-		
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(320,320);
+    glutCreateWindow("CG@DI-UM");
+        
 
 // Required callback registry 
-	glutDisplayFunc(renderScene);
-	glutIdleFunc(renderScene);
-	glutReshapeFunc(changeSize);
+    glutDisplayFunc(renderScene);
+    glutIdleFunc(renderScene);
+    glutReshapeFunc(changeSize);
 
 // Callback registration for keyboard processing
-	glutKeyboardFunc(processKeys);
-	glutMouseFunc(processMouseButtons);
-	glutMotionFunc(processMouseMotion);
+    glutKeyboardFunc(processKeys);
+    glutMouseFunc(processMouseButtons);
+    glutMotionFunc(processMouseMotion);
 
     //  Devil - get access to IL- main DevIL library functions(https://moddb.fandom.com/wiki/DevIL:Tutorials:Basics)
     ilInit();
@@ -275,8 +275,8 @@ int main(int argc, char **argv) {
     init();
 
 // enter GLUT's main cycle
-	glutMainLoop();
+    glutMainLoop();
 
-	return 0;
+    return 0;
 }
 

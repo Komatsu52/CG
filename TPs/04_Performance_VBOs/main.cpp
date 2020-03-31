31b141663775bd1ad029fbad444dea53fbb4b7fc
marioreal <marioreal@users.noreply.github.com>
2020-03-31 04:06:39 +0100

    TPs 4(Performance_VBOs) e 6(Terrain) com código comentado.

---
+++ TPs/04_Performance_VBOs/main.cpp
@@ -0,0 +1,383 @@
+#include <stdio.h>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include <vector>
+#endif
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+//para declarar só "vector" em vez de std::vector...
+using std::vector;
+
+
+// FPS - mostrador ****** variáveis ************************************
+int frame = 0;
+int time = 0;
+int timebase = 0;
+// *********************************************************
+
+// A variável vertices deverá ser global já que será necessária na função renderScene. A variável
+// vertices ficará com o valor 1 por ser o primeiro VBO a ser criado. Este valor representa o número do
+// “slot” alocado e é usado sempre que nos quisermos referir ao VBO. A variável verticeCount, também
+// global, irá guardar o número total de vértices. Iremos precisar de ambas estas variáveis para desenhar o
+// triângulo na função renderScene.//
+GLuint vertices, verticeCount;
+
+// criar um vector com os dados dos pontos
+//vector<float> p;
+
+
+float alfa = 0.0f, beta = 0.0f, radius = 5.0f;
+float camX, camY, camZ;
+
+void spherical2Cartesian() {
+
+    camX = radius * cos(beta) * sin(alfa);
+    camY = radius * sin(beta);
+    camZ = radius * cos(beta) * cos(alfa);
+}
+
+void changeSize(int w, int h) {
+
+    // Prevent a divide by zero, when window is too short
+    // (you cant make a window with zero width).
+    if(h == 0)
+        h = 1;
+
+    // compute window's aspect ratio
+    float ratio = w * 1.0 / h;
+
+    // Set the projection matrix as current
+    glMatrixMode(GL_PROJECTION);
+    // Load Identity Matrix
+    glLoadIdentity();
+
+    // Set the viewport to be the entire window
+    glViewport(0, 0, w, h);
+
+    // Set perspective
+    gluPerspective(45.0f ,ratio, 1.0f ,1000.0f);
+
+    // return to the model view matrix mode
+    glMatrixMode(GL_MODELVIEW);
+}
+
+/*-----------------------------------------------------------------------------------
+Método normal da função cilindro (sem VBOs) - usando glVertex3f
+-----------------------------------------------------------------------------------*/
+void cylinder(float radius, float height, int sides) {
+    int i;
+    float step;
+
+    step = 360.0 / sides;
+
+    glBegin(GL_TRIANGLES);
+
+    // top
+    for (i = 0; i < sides; i++) {
+        glVertex3f(0, height*0.5, 0);
+        glVertex3f(cos(i * step * M_PI / 180.0)*radius, height*0.5, -sin(i * step *M_PI / 180.0)*radius);
+        glVertex3f(cos((i+1) * step * M_PI / 180.0)*radius, height*0.5, -sin((i + 1) * step *M_PI / 180.0)*radius);
+    }
+
+    // bottom
+    for (i = 0; i < sides; i++) {
+        glVertex3f(0, -height*0.5, 0);
+        glVertex3f(cos((i + 1) * step * M_PI / 180.0)*radius, -height*0.5, -sin((i + 1) * step *M_PI / 180.0)*radius);
+        glVertex3f(cos(i * step * M_PI / 180.0)*radius, -height*0.5, -sin(i * step *M_PI / 180.0)*radius);
+    }
+
+    // body
+    for (i = 0; i <= sides; i++) {
+
+        glVertex3f(cos(i * step * M_PI / 180.0)*radius, height*0.5, -sin(i * step *M_PI / 180.0)*radius);
+        glVertex3f(cos(i * step * M_PI / 180.0)*radius, -height*0.5, -sin(i * step *M_PI / 180.0)*radius);
+        glVertex3f(cos((i + 1) * step * M_PI / 180.0)*radius, height*0.5, -sin((i + 1) * step *M_PI / 180.0)*radius);
+
+        glVertex3f(cos(i * step * M_PI / 180.0)*radius, -height*0.5, -sin(i * step *M_PI / 180.0)*radius);
+        glVertex3f(cos((i + 1) * step * M_PI / 180.0)*radius, -height*0.5, -sin((i + 1) * step *M_PI / 180.0)*radius);
+        glVertex3f(cos((i + 1) * step * M_PI / 180.0)*radius, height*0.5, -sin((i + 1) * step *M_PI / 180.0)*radius);
+    }
+    glEnd();
+}
+
+/*-----------------------------------------------------------------------------------
+    Draw Cylinder with strips and fans (Sem VBOs)
+-----------------------------------------------------------------------------------*/
+// explicações basícas 1 tipos de triângulos : https://tinyurl.com/rlgnb5n
+// explicações basícas 2: https://tinyurl.com/wymm8sd
+void cylinder0(float radius, float height, int sides) {
+
+    int i;
+    float step;
+
+    step = 360.0/sides;
+
+    glColor3f(1,0,0);
+    glBegin(GL_TRIANGLE_FAN);
+
+    glVertex3f(0,height*0.5,0);
+    for (i=0; i <= sides; i++) {
+        glVertex3f(cos(i * step * M_PI/180.0)*radius,height*0.5,-sin(i * step *M_PI/180.0)*radius);
+    }
+    glEnd();
+
+    glColor3f(0,1,0);
+    glBegin(GL_TRIANGLE_FAN); // A primitiva GL_TRIANGLE_FAN é usada para se conectar vários triângulos em torno de um ponto central, tal como um leque (“fan”):
+
+    glVertex3f(0,-height*0.5,0);
+    for (i=0; i <= sides; i++) {
+        glVertex3f(cos(i * step * M_PI/180.0)*radius,-height*0.5,sin(i * step *M_PI/180.0)*radius);
+    }
+    glEnd();
+
+    glColor3f(0,0,1);
+    glBegin(GL_TRIANGLE_STRIP); //A primitiva GL_TRIANGLE_STRIP é usada para se desenhar uma tira de triângulos (triângulos conectados sequencialmente).
+
+    for (i=0; i <= sides; i++) {
+        glVertex3f(cos(i * step * M_PI/180.0)*radius, height*0.5,-sin(i * step *M_PI/180.0)*radius);
+        glVertex3f(cos(i * step * M_PI/180.0)*radius,-height*0.5,-sin(i * step *M_PI/180.0)*radius);
+    }
+    glEnd();
+}
+
+
+/*-----------------------------------------------------------------------------------
+    Cilindro com VBOs - sem indices
+-----------------------------------------------------------------------------------*/
+void prepareDataCylinder(float radius, float height, int sides) {
+
+    //// 1) criar um vector com para as coordenadas dos pontos --------------
+    vector<float> p;
+
+    int i;
+    float step;
+
+    step = 360.0 / sides;
+
+    //preenchimento do vector com os pontos do cilindro
+    // top
+    for (i = 0; i < sides; i++) {
+        p.push_back(0);
+        p.push_back(height*0.5);
+        p.push_back(0);
+
+        p.push_back(cos(i * step * M_PI / 180.0)*radius);
+        p.push_back(height*0.5);
+        p.push_back(-sin(i * step *M_PI / 180.0)*radius);
+
+        p.push_back(cos((i+1) * step * M_PI / 180.0)*radius);
+        p.push_back(height*0.5);
+        p.push_back(-sin((i + 1) * step *M_PI / 180.0)*radius);
+    }
+
+    // bottom
+    for (i = 0; i < sides; i++) {
+        p.push_back(0);
+        p.push_back(-height*0.5);
+        p.push_back(0);
+
+        p.push_back(cos((i + 1) * step * M_PI / 180.0)*radius);
+        p.push_back(-height*0.5);
+        p.push_back(-sin((i + 1) * step *M_PI / 180.0)*radius);
+
+        p.push_back(cos(i * step * M_PI / 180.0)*radius);
+        p.push_back(-height*0.5);
+        p.push_back(-sin(i * step *M_PI / 180.0)*radius);
+    }
+
+    // body
+    for (i = 0; i <= sides; i++) {
+        p.push_back(cos(i * step * M_PI / 180.0)*radius); p.push_back(height*0.5); p.push_back(-sin(i * step *M_PI / 180.0)*radius);
+        p.push_back(cos(i * step * M_PI / 180.0)*radius); p.push_back(-height*0.5); p.push_back(-sin(i * step *M_PI / 180.0)*radius);
+        p.push_back(cos((i + 1) * step * M_PI / 180.0)*radius); p.push_back(height*0.5); p.push_back(-sin((i + 1) * step *M_PI / 180.0)*radius);
+
+        p.push_back(cos(i * step * M_PI / 180.0)*radius); p.push_back(-height*0.5); p.push_back(-sin(i * step *M_PI / 180.0)*radius);
+        p.push_back(cos((i + 1) * step * M_PI / 180.0)*radius); p.push_back(-height*0.5); p.push_back(-sin((i + 1) * step *M_PI / 180.0)*radius);
+        p.push_back(cos((i + 1) * step * M_PI / 180.0)*radius); p.push_back(height*0.5); p.push_back(-sin((i + 1) * step *M_PI / 180.0)*radius);
+    }
+
+    // atualização do número de vértices/Pontos (vertice =  3 coordenadas x,y,z)
+    verticeCount = p.size() / 3;
+
+    //// 2) criar o VBO -----------------------------------------------------------------------------
+
+    glGenBuffers(1, &vertices); // Enable buffer vertices
+
+    //// 3) copiar o vector para a memória gráfica --------------------------------------------------
+    // copiar o vector para a memória gráfica
+    glBindBuffer(GL_ARRAY_BUFFER, vertices);
+    glBufferData(
+            GL_ARRAY_BUFFER, // tipo do buffer, só é relevante na altura do desenho
+            sizeof(float) * p.size(), // tamanho do vector em bytes
+            p.data(), // os dados do array associado ao vector
+            GL_STATIC_DRAW); // indicativo da utilização (estático e para desenho)
+}
+
+///// -----------------------------------------------------------------------------------
+////  Draw Cylinder
+void drawVBOCylinder() {
+    //  Bind and semantics
+    glBindBuffer(GL_ARRAY_BUFFER, vertices); //No excerto de código acima primeiro indicamos que vertices é o VBO activo. Aqui sim, o tipo do buffer é relevante. Para arrays com propriedades dos vértices, como as posições, o tipo deve ser GL_ARRAY_BUFFER.
+    glVertexPointer(3, GL_FLOAT, 0, 0); // Define a semântica dos dados no VBO. Ao copiarmos os dados, com a função glBufferData, não passámos nenhuma informação sobre a estrutura dos dados, neste caso que um vértice contem 3 floats. Poderiam ser 4 doubles, ou outra variante qualquer. A função glBufferData só copia bytes sem associar nenhuma semântica a esses bytes. A função glVertexPointer é usada para fornecer essa semântica (os dois últimos parâmetros não são relevantes para este exemplo). Neste caso estamos a dizer que um vértice é constituído por 3 floats.
+
+    //  Draw
+    glDrawArrays(GL_TRIANGLES, 0, verticeCount); // Desenho dos triângulos presentes no VBO. O segundo parâmetro indica o vértice inicial, e o último o número de vértices a desenhar.
+}
+//// -----------------------------------------------------------------------------------
+
+
+void renderScene(void) {
+
+    float pos[4] = { 1.0, 1.0, 1.0, 0.0 };
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    // clear buffers
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // set the camera
+    glLoadIdentity();
+    glLightfv(GL_LIGHT0, GL_POSITION, pos);
+    gluLookAt(camX, camY, camZ,
+              0.0, 0.0, 0.0,
+              0.0f, 1.0f, 0.0f);
+
+    //// ---> Testes ////////////////////////////////////////////////////////////////////////
+    //// cylinder sem vbos ------------------------------------------------------------------
+    //cylinder(1, 2, 20000);
+    //// cylinder sem vbos with strips and fans ---------------------------------------------
+    //cylinder0(1, 2, 20000);
+    //// cylinder com vbos ------------------------------------------------------------------
+    drawVBOCylinder();
+    /////////////////////////////////////////////////////////////////////////////////////////
+
+
+    // FPS ****** func ************************************
+    float fps;
+    char str[100];
+
+    frame++;
+    time = glutGet(GLUT_ELAPSED_TIME);
+    if (time - timebase > 1000) {
+        fps = frame*1000.0 / (time - timebase);
+        timebase = time;
+        frame = 0;
+        sprintf(str, "%f fps", fps);
+        glutSetWindowTitle(str);
+    }
+    // *********************************************************
+    // End of frame
+    glutSwapBuffers();
+}
+
+
+void processKeys(unsigned char c, int xx, int yy) {
+
+// put code to process regular keys in here
+
+}
+
+
+void processSpecialKeys(int key, int xx, int yy) {
+
+    switch (key) {
+
+        case GLUT_KEY_RIGHT:
+            alfa -= 0.1; break;
+
+        case GLUT_KEY_LEFT:
+            alfa += 0.1; break;
+
+        case GLUT_KEY_UP:
+            beta += 0.1f;
+            if (beta > 1.5f)
+                beta = 1.5f;
+            break;
+
+        case GLUT_KEY_DOWN:
+            beta -= 0.1f;
+            if (beta < -1.5f)
+                beta = -1.5f;
+            break;
+
+        case GLUT_KEY_PAGE_DOWN: radius -= 0.1f;
+            if (radius < 0.1f)
+                radius = 0.1f;
+            break;
+
+        case GLUT_KEY_PAGE_UP: radius += 0.1f; break;
+    }
+    spherical2Cartesian();
+    glutPostRedisplay();
+
+}
+
+
+void printInfo() {
+
+    printf("Vendor: %s\n", glGetString(GL_VENDOR));
+    printf("Renderer: %s\n", glGetString(GL_RENDERER));
+    printf("Version: %s\n", glGetString(GL_VERSION));
+
+    printf("\nUse Arrows to move the camera up/down and left/right\n");
+    printf("Home and End control the distance from the camera to the origin");
+
+    printf ("\n vertices %d\n",verticeCount); //----------------------------------------------------------------------------add
+}
+
+
+int main(int argc, char **argv) {
+
+// init GLUT and the window
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(800,800);
+    glutCreateWindow("CG@DI-UM");
+
+// Required callback registry
+    glutDisplayFunc(renderScene);
+    glutIdleFunc(renderScene); //add -----------------------
+    glutReshapeFunc(changeSize);
+
+// Callback registration for keyboard processing
+    glutKeyboardFunc(processKeys);
+    glutSpecialFunc(processSpecialKeys);
+
+#ifndef __APPLE__
+    glewInit();
+#endif
+
+//  OpenGL settings
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+
+    //glPolygonMode(GL_FRONT, GL_LINE);
+
+    spherical2Cartesian();
+
+    // Do once during initialization of the app
+    glEnableClientState(GL_VERTEX_ARRAY); 
+
+    ////Construir VBO || Esta função só é necessária chamar 1 vez
+    // preparação dos dados com vbos para cilindro , a função de desenho drawVBOCylinder(), na renderscene, irá buscar estes dados alocados.
+    prepareDataCylinder(1, 2, 20000);
+
+
+    printInfo();
+
+// enter GLUT's main cycle
+    glutMainLoop();
+
+// FPS ****** time *****************************************
+    time = glutGet(GLUT_ELAPSED_TIME);
+// *********************************************************
+
+    return 1;
+}
---
+++ TPs/06_Terrain/code/main.cpp
@@ -0,0 +1,282 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <vector>
+
+#include <IL/il.h>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include <IL/il.h>
+#endif
+
+float camX = 00, camY = 30, camZ = 40;
+int startX, startY, tracking = 0;
+
+int alpha = 0, beta = 45, r = 50;
+
+////Variaveis para VBOs ---------------------------------------- - - - -
+/* (GLuint: 32bits-Unsigned binary integer == GL_UNSIGNED_INT)
+   A variável buffers ficará com o valor 1 por ser o primeiro VBO a ser criado. Este valor representa o número do
+   “slot” alocado e é usado sempre que nos quisermos referir ao VBO.*/
+GLuint n_coords, buffers[1];
+
+/////Variaveis para carregar imagem----------------------------- - - - -
+unsigned int t, tw, th;    //t-ref img; tw-ref imgWidth; th-ref imgHeight
+unsigned char *imageData;  // Array linear onde vão ficar o valores dos pixeis da nossa imagem
+
+void changeSize(int w, int h) {
+
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window with zero width).
+	if(h == 0)
+		h = 1;
+
+	// compute window's aspect ratio 
+	float ratio = w * 1.0 / h;
+
+	// Reset the coordinate system before modifying
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	
+	// Set the viewport to be the entire window
+    glViewport(0, 0, w, h);
+
+	// Set the correct perspective
+	gluPerspective(45,ratio,1,1000);
+
+	// return to the model view matrix mode
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void carregarImagem(){
+    //The Num parameter specifies how many images you will load with DevIL.The Images parameter is set to the image names.
+    ilGenImages(1,&t);
+    //Vincular imagem 't' na devil internal image stack, para executar todas as operacoes subsequentes nessa imagem.
+    ilBindImage(t);
+    // terreno.jpg is the image containing our height map (leitura da imagem)
+    ilLoadImage((ILstring)"terreno.jpg");
+    // convert the image to single channel per pixel, with values ranging between 0 and 255
+    ilConvertImage(IL_LUMINANCE, IL_UNSIGNED_BYTE);
+    /*important: check tw and th values
+    // both should be equal to 256
+    // if not there was an error loading the image
+    // most likely the image could not be found*/
+    tw = ilGetInteger(IL_IMAGE_WIDTH);
+    th = ilGetInteger(IL_IMAGE_HEIGHT);
+    // imageData is a LINEAR array with the pixel values
+    imageData = ilGetData();
+}
+
+//Devolve altura(pixelValue) do terreno em determinada posição da matriz XZ
+float h(int x, int z){                                //x1  x2
+    //Numeros do imageData vão de 0 a 255 ||         z0|210|...|
+    // -> 0-255 scale:
+    //return (imageData[x + z*tw]);
+    //Note: scale the heights from 0-255 (pixel intensity) to 0 - 30 meters for a more appropriate rendering
+    // -> 0-30 scale:
+    //return ((imageData[x + z*tw]) * 30 / 255);
+    // -> 0-120 scale:
+    return ((imageData[x + z*tw]) * 120 / 255);
+}
+
+void Build_VBOterreno(float largura,float altura){
+
+    float meiaLarguraX = (float)tw/2;
+    float meiaAlturaZ = (float)th/2;
+
+    // 1) criar um array para as coordenadas dos pontos -------------- - - -
+    float *vertexB = NULL;
+    int size = 0;
+
+    //numero de coordenadas no array
+    n_coords = (tw - 1)*th * 6;
+
+    vertexB = (float*)malloc(sizeof(float) * n_coords);
+
+    //Activar Buffers
+    glEnableClientState(GL_VERTEX_ARRAY);
+
+
+    //preenchimento do array com os pontos do terreno
+    for (int x = 0; x < tw-1; x++) {
+        for (int z = 0; z < th; z++) {
+            vertexB[size++] = ((x+1) - meiaLarguraX);
+            vertexB[size++] = h(x + 1, z);
+            vertexB[size++] = (z - meiaAlturaZ);
+
+            vertexB[size++] = (x - meiaLarguraX);
+            vertexB[size++] = h(x , z);
+            vertexB[size++] = (z - meiaAlturaZ);
+        }
+    }
+
+    // 2) criação do VBO ------------------------------------------------ - - -
+    glGenBuffers(1, buffers); // Enable buffer "buffers" com 1 VBO neste caso
+
+    // 3) copiar o array para a memória gráfica ------------------------- - - -
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
+    glBufferData(GL_ARRAY_BUFFER, size * sizeof(float), vertexB, GL_STATIC_DRAW);
+}
+
+
+void init() {
+
+// 	Load the height map "terreno.jpg"
+    carregarImagem();
+// 	Build the vertex arrays
+    Build_VBOterreno(tw,th);
+
+// 	OpenGL settings
+    glEnable(GL_DEPTH_TEST);
+    //glEnable(GL_CULL_FACE);
+    glPolygonMode(GL_FRONT, GL_LINE);
+}
+
+void drawTerrain() {
+    // colocar aqui o código de desenho do terreno usando VBOs com TRIANGLE_STRIPS
+
+    glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
+    glVertexPointer(3, GL_FLOAT, 0, 0);
+
+    // Desenho dos triângulos presentes no VBO. O segundo parâmetro indica o vértice inicial, e o último o número de vértices a desenhar.
+    // Como estamos a desenhar no modo GL_TRIANGLE_STRIP temos de desenhar uma 'tira' de cada vez
+    for (int i=0; i<th; i++) {
+        glDrawArrays(GL_TRIANGLE_STRIP, i*tw*2, 2*tw);
+    }
+
+}
+
+
+void renderScene(void) {
+
+	float pos[4] = {-1.0, 1.0, 1.0, 0.0};
+
+	glClearColor(0.0f,0.0f,0.0f,0.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glLoadIdentity();
+	gluLookAt(camX, camY, camZ,
+		      0.0,0.0,0.0,
+			  0.0f,1.0f,0.0f);
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    ////desenho do terreno
+    drawTerrain();
+
+	// just so that it renders something before the terrain is built
+	// to erase when the terrain is ready
+    glColor3f(0.0f, 0.0f, 1.0f);
+	glutWireTeapot(2.0);
+
+// End of frame
+	glutSwapBuffers();
+}
+
+void processKeys(unsigned char key, int xx, int yy) {
+
+// put code to process regular keys in here
+}
+
+void processMouseButtons(int button, int state, int xx, int yy) {
+
+	if (state == GLUT_DOWN)  {
+		startX = xx;
+		startY = yy;
+		if (button == GLUT_LEFT_BUTTON)
+			tracking = 1;
+		else if (button == GLUT_RIGHT_BUTTON)
+			tracking = 2;
+		else
+			tracking = 0;
+	}
+	else if (state == GLUT_UP) {
+		if (tracking == 1) {
+			alpha += (xx - startX);
+			beta += (yy - startY);
+		}
+		else if (tracking == 2) {
+
+			r -= yy - startY;
+			if (r < 3)
+				r = 3.0;
+		}
+		tracking = 0;
+	}
+}
+
+void processMouseMotion(int xx, int yy) {
+
+	int deltaX, deltaY;
+	int alphaAux, betaAux;
+	int rAux;
+
+	if (!tracking)
+		return;
+
+	deltaX = xx - startX;
+	deltaY = yy - startY;
+
+	if (tracking == 1) {
+
+
+		alphaAux = alpha + deltaX;
+		betaAux = beta + deltaY;
+
+		if (betaAux > 85.0)
+			betaAux = 85.0;
+		else if (betaAux < -85.0)
+			betaAux = -85.0;
+
+		rAux = r;
+	}
+	else if (tracking == 2) {
+
+		alphaAux = alpha;
+		betaAux = beta;
+		rAux = r - deltaY;
+		if (rAux < 3)
+			rAux = 3;
+	}
+	camX = rAux * sin(alphaAux * 3.14 / 180.0) * cos(betaAux * 3.14 / 180.0);
+	camZ = rAux * cos(alphaAux * 3.14 / 180.0) * cos(betaAux * 3.14 / 180.0);
+	camY = rAux * 							     sin(betaAux * 3.14 / 180.0);
+}
+
+int main(int argc, char **argv) {
+
+// init GLUT and the window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
+	glutInitWindowPosition(100,100);
+	glutInitWindowSize(320,320);
+	glutCreateWindow("CG@DI-UM");
+		
+
+// Required callback registry 
+	glutDisplayFunc(renderScene);
+	glutIdleFunc(renderScene);
+	glutReshapeFunc(changeSize);
+
+// Callback registration for keyboard processing
+	glutKeyboardFunc(processKeys);
+	glutMouseFunc(processMouseButtons);
+	glutMotionFunc(processMouseMotion);
+
+    //  Devil - get access to IL- main DevIL library functions(https://moddb.fandom.com/wiki/DevIL:Tutorials:Basics)
+    ilInit();
+
+    glewInit();
+    init();
+
+// enter GLUT's main cycle
+	glutMainLoop();
+
+	return 0;
+}
+
